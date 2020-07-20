5b2df6bdbf8ac4a1d66ec9253f14fadad952b779
matrix <15941999082@163.com>
2020-07-20 09:27:05 +0800

    go

---
+++ Demo.go
@@ -0,0 +1,134 @@
+package main
+
+import "fmt"
+
+/*
+const (
+		a = "abc"
+		b = len(a)
+		c = unsafe.Sizeof(a)
+)
+
+
+type Books struct {
+	title string
+	author string
+	subject string
+	book_id int
+}
+*/
+
+func main() {
+	/*
+	fmt.Println("Hello, World!")
+	var bol bool = true
+	fmt.Println(bol)
+	var a, b, c string = "aaa", "bbb", "ccc"
+	fmt.Println(a, b, c)
+	var d int
+	d = 1
+	fmt.Println(d)
+	e := "shengluo" //不应该是声明过的变量
+	fmt.Println(e)
+	f, g, h := 1, 2, 3
+	fmt.Println(f, g, h)
+	
+	const LENGTH int = 10;
+	const WIDTH int = 5;
+	fmt.Println(LENGTH,WIDTH)
+	
+	const (
+		Unknow = 0
+		Femake = 1
+		Male = 2
+	)
+	
+	fmt.Println(Femake)
+	
+	
+	fmt.Println(a, b, c) // import 的时候必须使用和 如果不是用那么 会报错 const可以定义在方法之外
+	
+		
+	const (
+		a = iota
+		b
+		c
+		d = "ha"
+		e
+		f = "100"
+		g
+		h = iota
+		i
+	)
+	
+	fmt.Println(a, b, c, d, e, f, g, h, i)
+	
+	var a int = 1
+	var ptr *int
+	ptr = &a
+	fmt.Println(ptr)
+
+	// * 是指针变量 可以保存地址值 没有*的话保存的是一个整数
+	// & 是地址值
+	
+	
+	
+	str1, str2 := swap("abc", "def")
+	
+	fmt.Println(str1, str2)
+	
+	var arr [10] int
+	
+	
+	var a int = 10 // 定义变量
+	var ptr *int   // 定义指针变量 不能初始化
+	ptr = &a
+	
+	fmt.Println(a)
+	fmt.Println(&a) // 地址
+	fmt.Println(ptr) // 地址
+	fmt.Println(*ptr) // 使用指针访问值
+	
+	
+	var ptr *int
+	fmt.Println(ptr)
+	if ptr == nil {
+		fmt.Println("空指针")
+	}
+	
+	
+	fmt.Println(Books{"Go 语言", "www", "Go 教程",1})
+	fmt.Println(Books{title:"Go 语言", author:"aaa", subject:"go", book_id : 1})
+	fmt.Println(Books{author:"aaa", title:"Go 语言", subject:"go", book_id : 1})
+	*/
+	fmt.Println("go")
+	
+
+}
+
+/* func swap(x, y string) (string, string) {
+	return y, x
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
